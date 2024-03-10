901e6a5cdd9e5a7f2c81d2eb005830822f9cd0ef
varun325 <sharmavarun1300@gmail.com>
2024-03-10 23:04:10 +0530

    React from scratch

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+                use: ["file-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+        open: true,
+    },
+};
